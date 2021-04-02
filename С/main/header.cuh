8676637b29930055d1d03477e98399fe74320f3b
PorisulkiP <alex1228wow@gmail.com>
2021-04-02 13:35:09 +0300

    Fixed creating a dll from C CUDA

--- С/main/header.cuh
+++ С/main/header.cuh
@@ -1,34 +1,13 @@
 ﻿#pragma once
 
-#ifdef MATHLIBRARY_EXPORTS
-#define MATHLIBRARY_API __declspec(dllexport)
+#ifdef MAIN_EXPORTS
+#define MAIN_API __declspec(dllexport)
 #else
-#define MATHLIBRARY_API __declspec(dllimport)
+#define MAIN_API __declspec(dllimport)
 #endif
 
-// Рекуррентное соотношение Фибоначчи описывает последовательность F
-// где F(n) равно { n = 0, a
-// { n = 1, b
-// { n > 1, F(n-2) + F(n-1)
-// для некоторых начальных интегральных значений a и b.
-// Если последовательность инициализирована F(0) = 1, F(1) = 1,
-// то это соотношение порождает хорошо известное Фибоначчи
-// последовательность: 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+// Вывод в консоль сообщения о том, что всё работает
+__declspec(dllexport) extern "C" MAIN_API void print_info();
 
-// Инициализация последовательности отношений Фибоначчи
-// такие, что F(0) = a, F(1) = b.
-// Эта функция должна быть вызвана перед любой другой функцией.
-extern "C" MATHLIBRARY_API void fibonacci_init(
-    const unsigned long long a, const unsigned long long b);
-
-
-// Произведите следующее значение в последовательности.
-// Возвращает true при успешном выполнении и обновляет текущее значение и индекс;
-// false при переполнении оставляет текущее значение и индекс неизменными.
-extern "C" MATHLIBRARY_API bool fibonacci_next();
-
-// Get the current value in the sequence.
-extern "C" MATHLIBRARY_API unsigned long long fibonacci_current();
-
-// Get the position of the current value in the sequence.
-extern "C" MATHLIBRARY_API unsigned fibonacci_index();
+// Запуск вычислений
+__declspec(dllexport) extern "C" MAIN_API int calculate();
--- С/main/main.cu
+++ С/main/main.cu
@@ -1,56 +1,17 @@
-﻿// MathLibrary.cpp : Defines the exported functions for the DLL.
-//#include "stdafx.h" // use stdafx.h in Visual Studio 2017 and earlier
-#include <utility>
-#include <limits.h>
-//#include "MathLibrary.h"
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
 
-// DLL internal state variables:
-static unsigned long long previous_;  // Previous value, if any
-static unsigned long long current_;   // Current sequence value
-static unsigned index_;               // Current seq. position
+#include <stdio.h>
+#include "header.cuh"
 
-// Initialize a Fibonacci relation sequence
-// such that F(0) = a, F(1) = b.
-// This function must be called before any other function.
-void fibonacci_init(
-    const unsigned long long a,
-    const unsigned long long b)
+// Вывод в консоль сообщения о том, что всё работает
+void print_info()
 {
-    index_ = 0;
-    current_ = a;
-    previous_ = b; // see special case when initialized
+    printf("\nI'm Work!!!\n\n");
 }
 
-// Produce the next value in the sequence.
-// Returns true on success, false on overflow.
-bool fibonacci_next()
+int calculate()
 {
-    // check to see if we'd overflow result or position
-    if ((ULLONG_MAX - previous_ < current_) ||
-        (UINT_MAX == index_))
-    {
-        return false;
-    }
 
-    // Special case when index == 0, just return b value
-    if (index_ > 0)
-    {
-        // otherwise, calculate next sequence value
-        previous_ += current_;
-    }
-    std::swap(current_, previous_);
-    ++index_;
-    return true;
-}
-
-// Get the current value in the sequence.
-unsigned long long fibonacci_current()
-{
-    return current_;
-}
-
-// Get the current index position in the sequence.
-unsigned fibonacci_index()
-{
-    return index_;
+    return 0;
 }
