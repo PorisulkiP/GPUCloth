f03f8420adab8572323c80386128175a065e4044
Victus <kirvlad1.3@gmail.com>
2021-04-12 15:28:30 +0300

    Checkboxes transformation to Panels

--- python/UI.py
+++ python/UI.py
@@ -70,7 +70,47 @@ class MySettings(PropertyGroup):
         min = 0.001,
         max = 30.0
         )
+
+    my_float_distance : FloatProperty(
+        name = "Set a value",
+        description = "A float property",
+        default = 0.015,
+        min = 0.001,
+        max = 30.0
+        ) 
         
+    my_float_distance_self : FloatProperty(
+        name = "Set a value",
+        description = "A float property",
+        default = 0.015,
+        min = 0.001,
+        max = 30.0
+        ) 
+    
+    my_float_friction : FloatProperty(
+        name = "Set a value",
+        description = "A float property",
+        default = 5.000,
+        min = 0.001,
+        max = 30.0
+        ) 
+
+    my_float_impulse : FloatProperty(
+        name = "Set a value",
+        description = "A float property",
+        default = 0.000,
+        min = 0.000,
+        max = 30.0
+        ) 
+
+    my_float_impulse_self : FloatProperty(
+        name = "Set a value",
+        description = "A float property",
+        default = 0.000,
+        min = 0.000,
+        max = 30.0
+        ) 
+
     my_string : StringProperty(
         name = "Set a value",
         description = "A string property",
@@ -85,6 +125,7 @@ class UV_PT_GPUCloth(Panel):
     bl_category = "My Category"
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
+    bl_options = {'DEFAULT_CLOSED'}
 
     def draw(self, context):
         layout = self.layout
@@ -96,25 +137,66 @@ class UV_PT_GPUCloth(Panel):
         layout.prop(mytool, "my_float_speed", text="Speed Multiplier")
         layout.prop(mytool, "my_float_air", text="Air Viscosity")
 
-        layout.prop(mytool, "my_bool_object_coll", text="Object Collisions")
-        layout.prop(mytool, "my_bool_self_coll", text="Self Collisions")
 
-        # check if bool property is enabled
-        if (mytool.my_bool_object_coll == True):
-            print ("Property Enabled")
-        else:
-            print ("Property Disabled")
+class UV_PT_GPUCloth_ObjColl(Panel):
+    bl_idname = "UV_PT_GPUCloth_ObjColl"
+    bl_label = ""
+    bl_parent_id = "UV_PT_GPUCloth"
+    bl_category = "My Category"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_options = {'DEFAULT_CLOSED'}
 
-        if (mytool.my_bool_self_coll == True):
-            print ("Property Enabled")
-        else:
-            print ("Property Disabled")
+    def draw_header(self,context):
+        self.layout.prop(context.scene.render, "use_border", text="Object Collizions")
+        
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        mytool = scene.my_tool
 
+        # display the properties
+        layout.prop(mytool, "my_float_distance", text="Distance (m)")
+        layout.prop(mytool, "my_float_impulse", text="Impulse Clamp")
+
+class UV_PT_GPUCloth_SelfColl(Panel):
+    bl_idname = "UV_PT_GPUCloth_SelfColl"
+    bl_label = ""
+    bl_parent_id = "UV_PT_GPUCloth"
+    bl_category = "My Category"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_options = {'DEFAULT_CLOSED'}
+
+    def draw_header(self,context):
+        self.layout.prop(context.scene.render, "use_border", text="Self Collizions")
+        
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        mytool = scene.my_tool
+
+        # display the properties
+        layout.prop(mytool, "my_float_friction", text="Friction")
+        layout.prop(mytool, "my_float_distance_self", text="Distance (m)")
+        layout.prop(mytool, "my_float_impulse_self", text="Impulse Clamp")
 #     --- Registration UI Panel ---
+
+#class Open_Panel_GPUCloth(bpy.types.Operator):
+#    bl_idname = "Open_Panel_GPUCloth"
+#    bl_label = "GPUCloth"
+#    bl_options = {"REGISTER", "UNDO"}
+# 
+#    def execute(self, context):
+#        UV_PT_GPUCloth.bl_options = "True"
+#        return {"FINISHED"}
 
 classes = (
     MySettings,
     UV_PT_GPUCloth,
+    UV_PT_GPUCloth_ObjColl,
+    UV_PT_GPUCloth_SelfColl,
+#    Open_Panel_GPUCloth
 )
 
 def register():
