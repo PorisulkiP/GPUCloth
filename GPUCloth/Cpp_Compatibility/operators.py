e016e03a21d8cd279994ce81437243f687b8fc11
PorisulkiP <alex1228wow@gmail.com>
2026-07-26 01:53:21 +0300

    test(blender): prove shear stiffness parity

--- GPUCloth/Cpp_Compatibility/operators.py
+++ GPUCloth/Cpp_Compatibility/operators.py
@@ -134,7 +134,9 @@ def _configure_material_features(dll, clmd, settings):
     )
     header = cast(
         pointer(config), POINTER(CType.GPUClothFeatureConfigHeader))
-    for feature in (CType.GPUCLOTH_FEATURE_STRETCH,):
+    for feature in (
+            CType.GPUCLOTH_FEATURE_STRETCH,
+            CType.GPUCLOTH_FEATURE_SHEAR):
         config.header.feature_id = feature
         result = int(dll.SIM_configure_cloth_feature(clmd, header))
         if result != CType.GPUCLOTH_ABI_OK:
